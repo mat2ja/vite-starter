c3a8455093c87c014036acb9ca93134d3df2526c
Matija Osrecki <matija.osrecki@gmail.com>
2021-03-15 18:37:35 +0100

    :bug:

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="flex flex-col items-center gap-8">
-		<h1 class="text-9xl text-green-600">Not found! 👿</h1>
+		<h1 class="text-9xl text-red-500">Not found! 👿</h1>
 		<router-link to="/" class="text-white text-xl">Go home</router-link>
 	</div>
 </template>
